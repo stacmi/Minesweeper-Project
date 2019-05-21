b84c6ea831cbe5e79307e1e6616dbe345ff98718
Michael Stacy <octagonprogramming@gmail.com>
2019-05-20 21:18:45 -0700

    changes to board class

--- board_class.py
+++ board_class.py
@@ -1,25 +1,25 @@
 #this file is for the board class
 
-class Board:
+class Board(object):
     #constructor to initialize a board for the object
-    def __init__(self, size):
+    def __init__(self, val):
         self.board = []
-        self.size = size
+        self.size = val
 
     #create blank board for playing
-    def create_blank_board():
+    def create_blank_board(self):
         #generate blank board to be used
         blank_board = [['-' for i in range(self.size)] for i in range(self.size)]
         return blank_board
 
     #display the playing board
-    def display_board():
-        self.board = create_blank_board()
+    def display_board(self):
+        self.board = self.create_blank_board(self)
         for row in self.board:
             print(row)
 
 def main():
-    board = Board(5)
-    board.display_board()
+    board1 = Board(5)
+    board1.display_board(board1)
 
 main()
